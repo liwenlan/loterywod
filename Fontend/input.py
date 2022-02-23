216623d77d6308a1e8b57e58c1df896823171e1c
liwenlan <liwenlan2014@163.com>
2022-02-23 17:40:03 +0800

    change file structure

--- Fontend/input.py
+++
@@ -1,8 +0,0 @@
-
-class Input:
-    actionConfig = {"heart": "", "spade": "", "club": "", "diamond": "", "bonus": ""}
-    team = [0, 0, 0, 0, 0]
-
-
-    def setTeam(self, group):
-        pass
--- Fontend/inputConfig.py
+++
@@ -1,8 +0,0 @@
-class InputStruc:
-    def __init__(self):
-        self.heart = {'burpee': 3}
-        self.spade = {'jump': 4}
-        self.club = {'row': 9}
-        self.diamond = {'run': 6}
-        self.bonus = {'hit': 30}
-        self.team = (0, 0, 0, 0, 0)
